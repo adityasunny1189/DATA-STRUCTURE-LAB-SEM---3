775dc716efa0f83160bd41ee653989beb90b22bd
adityasunny1189 <adityapathak1189@gmail.com>
2019-08-30 22:26:50 +0530

    lab1

--- lab1_employee_file.c
+++ lab1_employee_file.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 struct Employee
 {
@@ -15,11 +16,12 @@ void search_data(FILE *fp,char str[]);
 int main()
 {
     FILE *fp;
-    printf("Welcome to employee datasheet\n");
-    printf("Choose your choice\n");
-    printf("1.Read Data\n2.Write Data\n3.Search Data\n4.Exit\n");
     int choice;
     char eid[20];
+    printf("Welcome to employee datasheet\n");
+    printf("Choose your choice\n");
+    printf("1.Read Data\n2.Display Data\n3.Search Data\n4.Exit\n");
+    scanf("%d",&choice);
     switch(choice)
     {
         case 1:display_data(fp);
@@ -30,5 +32,44 @@ int main()
                 scanf("%s",eid);
                 search_data(fp,eid);
                 break;
+        case 4:printf("Thanks\n");
+                break;
+        default:printf("Invalid command\n");
+    }
+}
+
+void read_data(FILE *fp)
+{
+    fp = fopen("Employee.dat","W");
+    if(fp == NULL)
+    {
+        printf("Error 404\n");
+        return;
+    }
+}
+
+void display_data(FILE *fp)
+{
+    char ch;
+    fp = fopen("Employee.dat","r");
+    if(fp == NULL)
+    {
+        printf("Error 404\n");
+        return;
+    }
+    while(ch != EOF)
+    {
+        ch = getc(fp);
+        printf("%c",ch);
+    }
+}
+
+void search_data(FILE *fp,char str[])
+{
+    fp = fopen("Employee.dat","r");
+    if(fp == NULL)
+    {
+        printf("Error 404\n");
+        return;
     }
 }
