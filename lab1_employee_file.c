dcab30af7d1b2fa6e5119f67992b2a9d4c9441dd
adityasunny1189 <adityapathak1189@gmail.com>
2019-09-01 11:46:28 +0530

    lab1

--- lab1_employee_file.c
+++ lab1_employee_file.c
@@ -5,11 +5,11 @@ struct Employee
 {
     char name[20];
     char id[20];
+    char dept[10];
     int salary;
-    int age;
 }E1;
 
-void read_data(FILE *fp);
+void Write_data(FILE *fp);
 void display_data(FILE *fp);
 void search_data(FILE *fp,char str[]);
 
@@ -20,13 +20,13 @@ int main()
     char eid[20];
     printf("Welcome to employee datasheet\n");
     printf("Choose your choice\n");
-    printf("1.Display Data\n2.Read Data\n3.Search Data\n4.Exit\n");
+    printf("1.Display Data\n2.Write Data\n3.Search Data\n4.Exit\n");
     scanf("%d",&choice);
     switch(choice)
     {
         case 1:display_data(fp);
                 break;
-        case 2:read_data(fp);
+        case 2:Write_data(fp);
                 break;
         case 3:printf("Enter the employee id to search\n");
                 scanf("%s",eid);
@@ -38,21 +38,26 @@ int main()
     }
 }
 
-void read_data(FILE *fp)
+void Write_data(FILE *fp)
 {
-    fp = fopen("Employee.dat","W");
+    fp = fopen("Employee.txt","a");
     if(fp == NULL)
     {
         printf("Error 404\n");
         return;
     }
-    fread(&E1,sizeof(struct Employee),1,fp);
+    printf("Enter name id Department and Salary\n");
+    scanf("%s%s%s%d",E1.name,E1.id,E1.dept,&E1.salary);
+    // fwrite(&E1,sizeof(E1),1,fp);
+    fprintf(fp,"%s\t%s\t%s\t%d\n",E1.name,E1.id,E1.dept,E1.salary);
+    printf("\n");
+    fclose(fp);
 }
 
 void display_data(FILE *fp)
 {
     char ch;
-    fp = fopen("Employee.dat","r");
+    fp = fopen("Employee.txt","r");
     if(fp == NULL)
     {
         printf("Error 404\n");
@@ -60,16 +65,21 @@ void display_data(FILE *fp)
     }
     while(ch != EOF)
     {
-        ch = getc(fp);
+        ch = getc(fp);             
         printf("%c",ch);
     }
+    //file can also be read in this way
+    // while(fread(&E1,sizeof(struct Employee),1,fp) == 1)
+    // {
+    //     printf("%s%s%s%d",E1.name,E1.id,E1.dept,E1.salary);
+    // }
     printf("\n");
     fclose(fp);
 }
 
 void search_data(FILE *fp,char str[])
 {
-    fp = fopen("Employee.dat","r");
+    fp = fopen("Employee.txt","r");
     if(fp == NULL)
     {
         printf("Error 404\n");
