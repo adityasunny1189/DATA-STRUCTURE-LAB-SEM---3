8471c58437c2d7881ca063a30016b8c79f7e0468
adityasunny1189 <adityapathak1189@gmail.com>
2019-09-05 14:28:29 +0530

    left shift

--- expression.c
+++ expression.c
@@ -1,7 +1,34 @@
 #include<stdio.h>
 int main()
 {
-    int x=2,y=4,z=5;
-    int res = x y + z - ;
-    printf("%d",res);
+    int i,j,m,n,temp,arr[1000];
+    printf("Enter the no of elements in the array: ");
+    scanf("%d",&m);
+    printf("Enter the no of left shift: ");
+    scanf("%d",&n);
+    printf("Enter the array elements\n");
+    for(i=0;i<m;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("Printed array is\n");
+    for(i=0;i<m;i++)
+    {
+        printf("%d\t",arr[i]);
+    }
+    printf("Applying left shift algorithm\n");
+    for(j=0;j<n;j++)
+    {
+        temp = arr[0];
+        for(i=1;i<m;i++)
+        {
+            arr[i-1] = arr[i];
+        }
+        arr[m-1] = temp;
+    }
+    printf("Shifted array is\n");
+    for(i=0;i<m;i++)
+    {
+        printf("%d\t",arr[i]);
+    }
 }
