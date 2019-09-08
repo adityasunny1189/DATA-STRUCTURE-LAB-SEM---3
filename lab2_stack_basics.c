3fd9a4e57adc504f97357876d4afbaaffbe5b66c
adityasunny1189 <adityapathak1189@gmail.com>
2019-09-08 23:00:50 +0530

    stack implementation in array

--- lab2_stack_basics.c
+++ lab2_stack_basics.c
@@ -7,31 +7,70 @@ struct stack
 {
     int arr[MAX_SIZE];
     int sp;
-}S1;
+};
 
-void push(struct stack,int i);
-int pop(struct stack);
-void display(struct stack);
+int isFullStack(struct stack *S1)
+{
+    if(S1->sp == MAX_SIZE - 1)
+        return 1;
+    else
+        return 0;
+}
+
+int isEmptyStack(struct stack *S1)
+{
+    if(S1->sp == -1)
+        return 1;
+    else
+        return 0;
+}
+
+void push(struct stack *S1,int i)
+{
+    if(isFullStack(S1))
+        printf("OVERFLOW\n");
+    S1->arr[++S1->sp] == i;
+}
+
+int pop(struct stack *S1)
+{
+    if(isEmptyStack(S1))
+        printf("UNDERFLOW\n");
+    return (S1->arr[S1->sp--]);
+}
+
+void display(struct stack *S1)
+{
+    for(int i = 0; i <= S1->sp ; i++)
+        printf("%d\t",S1->arr[i]);
+}
 
 int main()
 {
+    struct stack S1;
     S1.sp = -1;
+    char adi = 'y';
     printf("STACK IMPLEMENTATION\n");
-    printf("Enter your choice\n1.PUSH Element in stack\n2.POP Element\n3.DISPLAY stack\n");
-    int choice,element;
-    scanf("%d",&choice);
-    switch(choice)
+    while(adi == 'y')
     {
-        case 1:printf("Enter element to push\n");
-                scanf("%d",&element);
-                push(S1,element);
-                break;
-        case 2:printf("poped element from stack is %d",pop(S1));
-                break;
-        case 3:printf("Stack elements are \n");
-                display(S1);
-                break;
-        default:printf("INVALID CHOICE\n");
+        printf("Enter your choice\n1.PUSH Element in stack\n2.POP Element\n3.DISPLAY stack\n");
+        int choice,element;
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 1:printf("Enter element to push\n");
+                    scanf("%d",&element);
+                    push(&S1,element);
+                    break;
+            case 2:printf("poped element from stack is %d",pop(&S1));
+                    break;
+            case 3:printf("Stack elements are \n");
+                    display(&S1);
+                    break;
+            default:printf("INVALID CHOICE\n");
+        }
+        printf("\nDo you want to continue: ");
+        scanf(" %c",&adi);
     }
     return 0;
 }
