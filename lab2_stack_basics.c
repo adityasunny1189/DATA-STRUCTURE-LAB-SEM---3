6add99fa3cf1d01aeeae8a4eec774ce5ace77836
adityasunny1189 <adityapathak1189@gmail.com>
2019-09-10 05:38:33 +0530

    stack final

--- lab2_stack_basics.c
+++ lab2_stack_basics.c
@@ -3,54 +3,54 @@
 
 #define MAX_SIZE 10
 
-int temp;
-
 struct stack
 {
+    int top;
     int arr[MAX_SIZE];
-    int sp;
-};
+}S1;
 
-int isFullStack(struct stack *S1)
+int isFull(struct stack *S1)
 {
-    if(S1->sp == MAX_SIZE - 1)
+    if(S1->top == MAX_SIZE - 1)
         return 1;
     else
         return 0;
 }
 
-int isEmptyStack(struct stack *S1)
+int isEmpty(struct stack *S1)
 {
-    if(S1->sp == -1)
+    if(S1->top == -1)
         return 1;
     else
         return 0;
 }
 
-void push(struct stack *S1,int i)
+void push(struct stack *S1, int ele)
 {
-    if(isFullStack(S1))
-        printf("OVERFLOW\n");
-    S1->arr[++S1->sp] == i;
+    if(isFull(S1))
+        printf("STACK OVERFLOW\n");
+    else
+        S1->arr[++S1->top] = ele;
 }
 
-void pop(struct stack *S1)
+int pop(struct stack *S1)
 {
-    if(isEmptyStack(S1))
-        printf("UNDERFLOW\n");
-    temp  = S1->arr[S1->sp--];
+    if(isEmpty(S1))
+        printf("STACK UNDERFLOW\n");
+    else
+        return (S1->arr[S1->top--]);
 }
 
 void display(struct stack *S1)
 {
-    for(int i = 0; i <= S1->sp ; i++)
+    for(int i = 0 ; i <= S1->top ; i++)
         printf("%d\t",S1->arr[i]);
+    printf("\n");
 }
 
 int main()
 {
-    struct stack S1;
-    S1.sp = -1;
+    S1.top = -1;
     char adi = 'y';
     printf("STACK IMPLEMENTATION\n");
     while(adi == 'y')
@@ -64,8 +64,7 @@ int main()
                     scanf("%d",&element);
                     push(&S1,element);
                     break;
-            case 2:pop(&S1);
-                   printf("poped element from stack is %d",temp);
+            case 2:printf("poped element from stack is %d",pop(&S1));
                    break;
             case 3:printf("Stack elements are \n");
                     display(&S1);
@@ -76,5 +75,84 @@ int main()
         scanf(" %c",&adi);
     }
     return 0;
-}
+}                                                // #include<stdio.h>
+                                                // #include<stdlib.h>
+
+                                                // #define MAX_SIZE 10
+
+                                                // struct stack
+                                                // {
+                                                //     int arr[MAX_SIZE];
+                                                //     int sp;
+                                                // }S1;
+
+                                                // int isFullStack(struct stack *S1)
+                                                // {
+                                                //     if(S1->sp == MAX_SIZE - 1)
+                                                //         return 1;
+                                                //     else
+                                                //         return 0;
+                                                // }
+
+                                                // int isEmptyStack(struct stack *S1)
+                                                // {
+                                                //     if(S1->sp == -1)
+                                                //         return 1;
+                                                //     else
+                                                //         return 0;
+                                                // }
+
+                                                // void push(struct stack *S1,int i)
+                                                // {
+                                                //     if(isFullStack(S1))
+                                                //         printf("OVERFLOW\n");
+                                                //     else
+                                                //         S1->arr[++S1->sp] == i;
+                                                // }
+
+                                                // int pop(struct stack *S1)
+                                                // {
+                                                //     if(isEmptyStack(S1))
+                                                //         printf("UNDERFLOW\n");
+                                                //     else
+                                                //         return (S1->arr[S1->sp--]);
+                                                // }
+
+                                                // void display(struct stack *S1)
+                                                // {
+                                                //     for(int i = 0; i <= S1->sp ; i++)
+                                                //         printf("%d\t",S1->arr[i]);
+                                                // }
+
+                                                // int main()
+                                                // {
+                                                //     S1.sp = -1;
+                                                //     char adi = 'y';
+                                                //     printf("STACK IMPLEMENTATION\n");
+                                                //     while(adi == 'y')
+                                                //     {
+                                                //         printf("Enter your choice\n1.PUSH Element in stack\n2.POP Element\n3.DISPLAY stack\n");
+                                                //         int choice,element;
+                                                //         scanf("%d",&choice);
+                                                //         switch(choice)
+                                                //         {
+                                                //             case 1:printf("Enter element to push\n");
+                                                //                     scanf("%d",&element);
+                                                //                     push(&S1,element);
+                                                //                     break;
+                                                //             case 2:printf("poped element from stack is %d",pop(&S1));
+                                                //                    break;
+                                                //             case 3:printf("Stack elements are \n");
+                                                //                     display(&S1);
+                                                //                     break;
+                                                //             default:printf("INVALID CHOICE\n");
+                                                //         }
+                                                //         printf("\nDo you want to continue: ");
+                                                //         scanf(" %c",&adi);
+                                                //     }
+                                                //     return 0;
+                                                // }
+
+
+
 
