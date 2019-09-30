9d5ee74deb3854f05a53aa9db36afe57d1a249a8
adityasunny1189 <adityapathak1189@gmail.com>
2019-09-30 10:56:33 +0530

    Queue

---
+++ circular_queue_home.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Queue
+{
+    char string[30];
+    int front,rear;
+};
+
+int isEmpty(struct Queue *Q)
+{
+
+}
+
+int isFull(struct Queue *Q)
+{
+
+}
+
+void Equeue(struct Queue *Q, int no)
+{
+
+}
+
+void Dqueue(struct Queue *Q)
+{
+
+}
+
+void Display(struct Queue *Q)
+{
+
+}
+
+int main()
+{
+    printf("Circular Queue Implementation\n");
+    
+}
