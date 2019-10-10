656897c74d785c06bbef76a762448f9fc7666ecb
adityasunny1189 <adityapathak1189@gmail.com>
2019-10-10 23:40:25 +0530

    C under Linux

--- C-under_linux_getpid.c
+++ C-under_linux_getpid.c
@@ -2,7 +2,8 @@
 #include<sys/types.h>
 int main()
 {
-    printf("Process ID : %d",getpid());
+    printf("Parent Process ID : %d",getpid());
+    printf("\nChild Process ID : %d",getppid());
     printf("\n");
     return 0;
 }
