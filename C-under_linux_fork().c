ba35f7fdcc79af5c6c4269f32999ba7ac5e2e83f
adityasunny1189 <adityapathak1189@gmail.com>
2019-10-11 15:33:46 +0530

    fork system call C under Linux

--- C-under_linux_fork().c
+++ C-under_linux_fork().c
@@ -4,24 +4,41 @@
 #include<unistd.h>
 int main()
 {
-    printf("Before fork() call this is parent\n");
-    // fork();
+    // printf("Before fork() call this is parent\n");
+    // // fork();
     
-    // //After this the instructions will be executed twice 
-    // //One by Child and other by Parent process
+    // // //After this the instructions will be executed twice 
+    // // //One by Child and other by Parent process
     
-    // printf("child process is created\n");   
+    // // printf("child process is created\n");   
 
-    int pid;
+    // int pid;
+    // pid = fork();
+    // if(pid == 0)
+    // {
+    //     printf("Inside Child Process\n");
+    // }
+    // else
+    // {
+    //     printf("Inside Parent Process\n");
+    // }
+
+    int pid ;
     pid = fork();
+
     if(pid == 0)
     {
-        printf("Inside Child Process\n");
+        printf("Child: In child process\n");
+        printf("Child: child process ID : %d\n",getppid());
+        printf("Child: Parent process ID : %d\n",getpid());
     }
     else
     {
-        printf("Inside Parent Process\n");
+        printf("Parent: In Parent Process\n");
+        printf("Parent: Parent process ID: %d\n",getpid());
+        printf("Parent: Child process ID: %d\n",getppid());
     }
+    
     
     return 0;
 }
