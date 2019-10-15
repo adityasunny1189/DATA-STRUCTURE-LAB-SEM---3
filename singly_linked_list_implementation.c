bb47be76932c0b9c67b0aea1ee48340170cfb453
adityasunny1189 <adityapathak1189@gmail.com>
2019-10-15 20:08:33 +0530

    singly linked list  implementation

--- singly_linked_list_implementation.c
+++ singly_linked_list_implementation.c
@@ -12,6 +12,11 @@ void insertElementInLinkedList(struct Node **head , int ele , int pos)
   int k = 1;
   struct Node *p , *q , *newNode;
   newNode = (struct Node *)malloc(sizeof(struct Node));
+  if(!newNode)
+  {
+    printf("Memory Allocation Error\n");
+    return ;
+  }
   p = *head;
   newNode->data = ele;
   if(pos == 1)
@@ -41,20 +46,55 @@ void insertElementInLinkedList(struct Node **head , int ele , int pos)
   newNode->next = NULL;
 }
 
-void displayLinkedList(struct Node *head)
+void displayLinkedList(struct Node **head)
 {
   struct Node *i;
-  i = head;
+  i = *head;
   while(i != NULL)
   {
-    printf("%d",i.data);
+    printf("%d",i->data);
     i = i->next;
   }
+}
+
+int deleteElementInLinkedList(struct Node **head, int pos)
+{
+  int k = 1, ele;
+  struct Node *p, *q;
+  p = *head;
+  if(pos == 1)
+  {
+    ele = p->data;
+    *head = p->next;
+    free(p);
+  }
+  else
+  {
+    while(k < pos)
+    {
+      k++;
+      q = p ;
+      p = p->next;
+    }
+    if(p == NULL)
+    {
+      printf("Position not found\n");
+      exit(1);
+    }
+    else
+    {
+      ele = p->data;
+      q->next = p->next;
+      free(p);
+    }
+  }
+  return ele;
 }
 
 int main()
 {
   struct Node *head;
+
   printf("SINGLY LINKED LIST IMPLEMENTATION\n");
   int choice;
   char cnt;
@@ -63,11 +103,25 @@ int main()
     scanf("%d",&choice);
     switch(choice)
     {
-      case 1:
+      case 1: printf("Choice to Insert no in Linked List\n");
+              printf("Enter no to Insert: ");
+              int ele, pos;
+              scanf("%d",&ele);
+              printf("Enter the position: ");
+              scanf("%d",&pos);
+              insertElementInLinkedList(&head, ele, pos);
+              break;
 
-      case 2:
+      case 2: printf("Choice to delete the node\n");
+              printf("Enter the position of node to delete: ");
+              int position;
+              scanf("%d",&position);
+              printf("Deleted Node Element is: %d",deleteElementInLinkedList(&head, position));
+              break;
 
-      case 3:
+      case 3: printf("Linked List is: ");
+              displayLinkedList(&head);
+              break;
 
       case 4: exit(0);
 
