47b4567254aed45e60494fee78ec203731eda552
adityasunny1189 <adityapathak1189@gmail.com>
2019-11-10 17:03:42 +0530

    linked list practice

--- compare_linked_lists.c
+++ compare_linked_lists.c
@@ -21,16 +21,19 @@ int compare(NODE *l1, NODE *l2) {
   NODE *list1, *list2;
   list1 = l1;
   list2 = l2;
-  while(list1 != NULL || list2 != NULL) {
+  while(list1 != NULL && list2 != NULL) {
     if(list1->data == list2->data) {
       list1 = list1->next;
       list2 = list2->next;
     }
-    if(list1 != NULL && list2 == NULL)
+    else {
       return 1;
-    else if(list1 == NULL && list2 != NULL)
-      return -1;
+    }
   }
+  if(list1 != NULL && list2 == NULL)
+    return 1;
+  else if(list1 == NULL && list2 != NULL)
+    return -1;
   return 0;
 }
 
@@ -39,6 +42,7 @@ void display(NODE *head) {
   p = head;
   while(p != NULL) {
     printf("%d-->",p->data);
+    p = p->next;
   }
   printf("\n");
 }
