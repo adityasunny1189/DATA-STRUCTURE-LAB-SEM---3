bfe4f12e0a024d3a8623bba9d153c40d63004bd7
adityasunny1189 <adityapathak1189@gmail.com>
2019-11-15 16:37:53 +0530

    polynomial addition of linked list

---
+++ polynomial_addition_of_linked_list.c
@@ -0,0 +1,102 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node {
+  int data;
+  int powX;
+  int powY;
+  struct node *next;
+};
+
+typedef struct node NODE;
+
+NODE *getnode() {
+  NODE *newNode;
+  newNode = (NODE *)malloc(sizeof(NODE));
+  return newNode;
+}
+
+void insert(NODE **head, int coef, int px, int py) {
+  NODE *newN;
+  newN = getnode();
+  newN->data = coef;
+  newN->powX = px;
+  newN->powY = py;
+  newN->next = *head;
+  *head = newN;
+}
+
+void display(NODE *head) {
+  while(head != NULL) {
+    printf(" %dx^%dy^%d +",head->data,head->powX,head->powY);
+    head = head->next;
+  }
+  printf("\n");
+}
+
+int search(NODE **temp, int coef, int px, int py) {
+  NODE *head;
+  head = *temp;
+  while(head != NULL) {
+    if(head->powX == px && head->powY == py)
+    {
+      (*temp)->data += coef;
+      return 1;
+    }
+    head = head->next;
+  }
+  return 0;
+}
+
+void add(NODE *P1, NODE *P2) {
+  NODE *temp;
+  temp = P1;
+  while(P2 != NULL) {
+    if(!search(&temp, P2->data, P2->powX, P2->powY))
+      insert(&temp, P2->data, P2->powX, P2->powY);
+    P2 = P2->next;
+  }
+  display(temp);
+}
+
+int main() {
+  int coef, px, py, choice;
+  char ch;
+  NODE *P1 = NULL, *P2 = NULL;
+  do {
+    printf("1.Add in Polynomial 1\n2.Add in Polynomial 2\n3.Display Polynomial 1\n4.Display Polynomial 2\n5.Add\nChoice: ");
+    scanf("%d",&choice);
+    switch(choice) {
+      case 1: printf("Enter the values\n");
+              printf("Coefficient: ");
+              scanf("%d",&coef);
+              printf("Power of X: ");
+              scanf("%d",&px);
+              printf("Power of Y: ");
+              scanf("%d",&py);
+              insert(&P1, coef, px, py);
+              break;
+
+      case 2: printf("Enter the values\n");
+              printf("Coefficient: ");
+              scanf("%d",&coef);
+              printf("Power of X: ");
+              scanf("%d",&px);
+              printf("Power of Y: ");
+              scanf("%d",&py);
+              insert(&P2, coef, px, py);
+              break;
+
+      case 3: display(P1);break;
+
+      case 4: display(P2);break;
+
+      case 5: add(P1, P2);break;
+
+      default:printf("Invalid Choice\n");
+    }
+    printf("Continue(y/n): ");
+    scanf(" %c",&ch);
+  } while(ch == 'y');
+  return 0;
+}
