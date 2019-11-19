1631a9c2d2103027810592d74c70b880f74c475a
adityasunny1189 <adityapathak1189@gmail.com>
2019-11-20 03:51:34 +0530

    internal preparation

--- BinarySearchTree.c
+++ BinarySearchTree.c
@@ -32,30 +32,89 @@ BST *insert(BST *root, int ele) {
   return root;
 }
 
-void inorder(BST root) {
-  if(root != NULL) {
+void inorder(BST *root) {
+  if(root) {
     inorder(root->left);
     printf("%d ",root->data);
     inorder(root->right);
   }
 }
 
-void preorder(BST root) {
-  if(root != NULL) {
+void preorder(BST *root) {
+  if(root) {
     printf("%d ",root->data);
     preorder(root->left);
     preorder(root->right);
   }
 }
 
-void postorder(BST root) {
-  if(root != NULL) {
+void postorder(BST *root) {
+  if(root) {
     postorder(root->left);
     postorder(root->right);
     printf("%d ",root->data);
   }
 }
 
-BST *delete(BST *root, int data) {
+BST *findMax(BST *root) {
+  if(root == NULL)
+    return NULL;
+  else if(root->right == NULL)
+    return root;
+  else
+    return findMax(root->right);
+}
 
+BST *delete(BST *root, int data) {
+  BST *temp;
+  if(root == NULL)
+    printf("Not present\n");
+  else if(root->data > data)
+    root->left = delete(root->left, data);
+  else if(root->data < data)
+    root->right = delete(root->right, data);
+  else {
+    if(root->left && root->right) {
+      temp = findMax(root->left);
+      root->data = temp->data;
+      root->left = delete(root->left, data);
+    }
+    else {
+      temp = root;
+      if(root->left == NULL)
+        root = root->right;
+      else if(root->right == NULL)
+        root = root->left;
+      free(temp);
+    }
+  }
+  return root;
+}
+
+int main() {
+  int choice,ele;
+  char ch;
+  BST *root = NULL;
+  do {
+    /* code */
+    printf("1.Insert node\n2.Delete node\n3.inorder\n4.preorder\n5.postorder\nchoice: ");
+    scanf("%d",&choice);
+    switch(choice) {
+      case 1: printf("Enter ele: ");
+              scanf("%d",&ele);
+              root = insert(root,ele);
+              break;
+
+      case 2: printf("Enter ele: ");
+              scanf("%d",&ele);
+              root = delete(root,ele);
+              break;
+
+      case 3: inorder(root);break;
+      case 4: preorder(root);break;
+      case 5: postorder(root);break;
+    }
+    printf("(y/n): ");
+    scanf(" %c",&ch);
+  } while(ch == 'y');
 }
