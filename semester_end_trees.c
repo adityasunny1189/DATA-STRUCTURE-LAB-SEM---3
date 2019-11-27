2b23da1ff61cc3cadff9cbe9fec734e8de8a3fd7
adityasunny1189 <adityapathak1189@gmail.com>
2019-11-27 11:47:25 +0530

    Binary Search Tree

---
+++ semester_end_trees.c
@@ -0,0 +1,96 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct BinarySearchTree {
+  int data;
+  struct BinarySearchTree *leftChild;
+  struct BinarySearchTree *rightChild;
+};
+
+typedef struct BinarySearchTree BST;
+
+BST *getnode() {
+  return (BST *)malloc(sizeof(BST));
+}
+
+int findChild(BST *root, int ele) {
+  if(root->data == ele)
+    return 1;
+  else if(root->data > ele)
+    return findChild(root->leftChild, ele);
+  else if(root->data < ele)
+    return findChild(root->rightChild, ele);
+  else
+    return 0;
+}
+
+int findMinimumElement(BST *root) {
+  if(root == NULL)
+    return 0;
+  else if(root->leftChild == NULL)
+    return root->data;
+  else
+    return findMinimumElement(root->leftChild);
+}
+
+int findMaximumElement(BST *root) {
+  if(root == NULL)
+    return 0;
+  else if(root->rightChild == NULL)
+    return root->data;
+  else
+    return findMaximumElement(root->rightChild);
+}
+
+void inorder(BST *root) {
+  if(root != NULL) {
+    inorder(root->leftChild);
+    printf("%d ",root->data);
+    inorder(root->rightChild);
+  }
+}
+
+void preorder(BST *root) {
+  if(root != NULL) {
+    printf("%d ",root->data);
+    preorder(root->leftChild);
+    preorder(root->rightChild);
+  }
+}
+
+void postorder(BST *root) {
+  if(root) {
+    postorder(root->leftChild);
+    postorder(root->rightChild);
+    printf("%d ",root->data);
+  }
+}
+
+BST *insert(BST *root, int ele) {
+  if(root == NULL) {
+    BST *newNode;
+    newNode = getnode();
+    if(!newNode) {
+      printf("Memory allocation error\n");
+      return;
+    }
+    else {
+      newNode->data = ele;
+      newNode->leftChild = newNode->rightChild = NULL;
+      root = newNode;
+    }
+  }
+  else {
+    if(ele < root->data)
+      root->leftChild = insert(root->leftChild, ele);
+    else if(ele > root->data)
+      root->rightChild = insert(root->rightChild, ele);
+    else
+      printf("Node already present so cant duplicate\n");
+  }
+  return root;
+}
+
+BST *delete(BST *root, int ele) {
+  if()
+}
