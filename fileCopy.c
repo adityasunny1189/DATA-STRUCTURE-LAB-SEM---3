352df3796c981394b23d627b79554440a39ef8ef
adityasunny1189 <adityapathak1189@gmail.com>
2019-12-15 17:53:55 +0530

    sem end

---
+++ fileCopy.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(int argc , char *argv[]) {
+    FILE *sourceFile, *destinationFile;
+    char ch;
+    if(argc != 3) {
+        printf("Improper number of arguments\n");
+        exit(1);
+    }
+    sourceFile = fopen(argv[1], "r");
+    if(sourceFile == NULL) {
+        printf("Source file unavailable\n");
+        exit(1);
+    }
+    destinationFile = fopen(argv[2], "w");
+    if(destinationFile == NULL) {
+        printf("Target not located\n");
+        fclose(sourcefile);
+        exit(1);
+    }
+    while(1) {
+        ch = fgetc(sourceFile);
+        if(ch == EOF) 
+            break;
+        else
+            fputc(ch, destinationFile);
+    }
+    fclose(sourceFile);
+    fclose(destinationFile);
+    return 0;
+}
